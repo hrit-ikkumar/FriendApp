87b0335bae68fbae8c31c78cfc0720aac7fa1f20
hrit-ikkumar <hritikkumar.email@gmail.com>
2020-10-15 12:59:51 +0530

    All set

--- app/src/main/java/com/example/randomfriend/MainActivity.java
+++ app/src/main/java/com/example/randomfriend/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.randomfriend;
 import java.util.Random;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mfrndBtn;
+    String random_friend;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,6 +17,22 @@ public class MainActivity extends AppCompatActivity {
         String friends[] = {"Mudit", "Ravi", "Abhinandan", "Chain", "Hritik"};
         Random rand = new Random();
         int index = rand.nextInt(5);
-        String random_friend = friends[index];
+        random_friend = friends[index];
+`       mfrndBtn = findViewById(R.id.button);
+        mfrndBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //mAuth.signOut();
+                sendUsertoFrnd();
+            }
+        });
     }
+    void sendUsertoFrnd()
+    {
+        Intent frnd = new Intent(MainActivity.this, frndActivity.class);
+        frnd.putExtra("EXTRA", random_friend);
+        startActivity(frnd);
+        finish();
+    }
+
 }
