9b904656a578bdc0d78807122300176aafbd179b
hrit-ikkumar <hritikkumar.email@gmail.com>
2020-10-15 13:42:54 +0530

    Friends Activity updated

--- app/src/main/java/com/example/randomfriend/MainActivity.java
+++ app/src/main/java/com/example/randomfriend/MainActivity.java
@@ -1,5 +1,5 @@
 package com.example.randomfriend;
-import java.util.Random;
+import java.util.*;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -14,11 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String friends[] = {"Mudit", "Ravi", "Abhinandan", "Chain", "Hritik"};
+        String friends[] = new String[]{"Mudit", "Ravi", "Abhinandan", "Chain", "Hritik"};
         Random rand = new Random();
         int index = rand.nextInt(5);
         random_friend = friends[index];
-`       mfrndBtn = findViewById(R.id.button);
+        mfrndBtn = findViewById(R.id.button);
         mfrndBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/randomfriend/frndActivity.java
+++ app/src/main/java/com/example/randomfriend/frndActivity.java
@@ -2,13 +2,27 @@ package com.example.randomfriend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class frndActivity extends AppCompatActivity {
+    private TextView printMe;
+    Context context;
+    public void ClassB(Context context){
+        this.context=context;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frnd);
+        Intent receive = getIntent();
+        String friend = receive.getStringExtra("EXTRA");
+        printMe = (TextView) ((Activity)context).findViewById(R.id.text);
+        printMe.setText(friend);
     }
 }
